77c4d915ec4ab022f34a0b13c2ba76ec5bc89d1e
pleasewander <stiltz.ryan@gmail.com>
2014-03-07 12:09:24 -0800

    made the js for the section about our majors, the navigation for that works

--- js/mbu.js
+++ js/mbu.js
@@ -1,3 +1,50 @@
+function hideAboutSection(){
+	$("#about1").hide();
+	$("#about2").hide();
+	$("#about3").hide();
+}
+
+function showAboutSection(){
+	$('#about1').fadeIn(1200, function(){$('#about2').fadeIn(1200, function(){$('#about3').fadeIn(1200, function(){});});});
+}
+
+function hideMajorSection(){
+
+	$('#foxinfo').hide();
+	$('#gorillainfo').hide();
+	$('#lioninfo').hide();
+	$('#squirrelinfo').hide();
+}
+
 $( document ).ready(function(){
+	hideAboutSection();
 	$('.sticky-element').waypoint('sticky');
+
+
+	$('#sectionabout').waypoint(function(){showAboutSection();});
+
+	hideMajorSection();
+	$('#foxinfo').slideToggle();
+
+	$('#foxbutton').click(function(){
+		hideMajorSection();
+		$('#foxinfo').slideToggle(1000);
+	});
+
+	$('#gorillabutton').click(function(){
+		hideMajorSection();
+		$('#gorillainfo').slideToggle(1000);
+	});
+	
+	$('#lionbutton').click(function(){
+		hideMajorSection();
+		$('#lioninfo').slideToggle(1000);
+	});
+
+	$('#squirrelbutton').click(function(){
+		hideMajorSection();
+		$('#squirrelinfo').slideToggle(1000);
+	});
+	
+	
 });
